ad60941ecca9e31030a36cf0817ba0b9d9e1a81d
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-05-04 15:19:18 +0300

    Initial Commit

---
+++ KeychainSwiftUI/KeychainSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainSwiftUIApp.swift
+//  KeychainSwiftUI
+//
+//  Created by Vasichko Anna on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct KeychainSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
